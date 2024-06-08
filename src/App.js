a8f451d054ff895d07f01dcf2849cc49ac8a3062
adithkrishnaor <adithkrishnaor@gmail.com>
2024-06-08 09:10:36 +0530

    added add user and navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import SearchUser from './components/SearchUser';
 import DeleteUser from './components/DeleteUser';
+import AddUser from './components/AddUser';
 
 function App() {
   return (
     <div>
       <SearchUser/>
       <DeleteUser/>
+      <AddUser/>
     </div>
   );
 }
---
+++ src/components/AddUser.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const AddUser = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                    <center>
+                            <h1>User Registration</h1>
+                        </center>
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Useer ID</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Name</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Username</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Email</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label">Address</label>
+                                <textarea name="" id="" className="form-control"></textarea>
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Phone Number</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Website</label>
+                                <input type="text" className="form-control" />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <center>
+                                <button className="btn btn-success">Submit</button>
+                                </center>
+                                <br />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </div>
+  )
+}
+
+export default AddUser
